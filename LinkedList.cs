62aca61a8ff469b389661db31176ffef4adac14b
CaptainYUNA <97616908+CaptainYUNA@users.noreply.github.com>
2022-02-07 02:23:15 +0900

    LinkedList 만들기 (1)

--- LinkedList.cs
+++ LinkedList.cs
@@ -4,123 +4,208 @@ using System.Text;
 
 namespace StudyChapter3
 {
-    public class Chapter3LinkeList
+    public class YUNALinkedList
     {
         public class Node
         {
-            public int data { get; set; }
-
-            public Node next { get; set; }
-
+            public int Data { get; private set; }
+            public Node Next { get; set; }
             public Node(int data)
             {
-                this.data = data;
-                next = null;
+                Data = data;
+                Next = null;
             }
         }
 
         public class LinkedList
         {
-            public Node head;
-            public Node GetLastNode()
+            public int Count { get => Count; }
+            public Node Head { get; private set; }
+
+            //FindLast
+            public Node FindLast()
             {
-                Node lastNode = head;
-                while (lastNode.next != null)
+                var lastNode = Head;
+
+                while (lastNode != null)
                 {
-                    lastNode = lastNode.next;
+                    lastNode = Head.Next;
                 }
 
                 return lastNode;
             }
 
-            public void InsertFront(int data)
+            //AddAfter
+            public void AddAfter(int previousData, int newData)
             {
-                Node node = new Node(data);
-                node.next = head;
-                head = node;
+                var preNode = Head;
+                var newNode = new Node(newData);
+
+                while (preNode.Data == previousData)
+                {
+                    preNode.Next = newNode;
+                }
             }
 
-            public void InsertLast(int data)
+            //AddBefore
+            public void AddBefore(int previousData, int newData)
             {
-                Node node = new Node(data);
+                var preNode = Head;
+                var newNode = new Node(newData);
+
+                while (preNode.Data == previousData)
+                {
+                    var nextNode = preNode.Next;
+
+                    preNode.Next = newNode;
+                    newNode.Next = nextNode;
+                }
+            }
+
+            //AddLast
+            public void AddLast(int newData)
+            {
+                var newNode = new Node(newData);
+                var head = Head;
 
                 if (head == null)
                 {
-                    head = node;
-                    return;
+                    head.Next = newNode;
                 }
 
-                Node lastNode = GetLastNode();
-                lastNode.next = node;
+                var lastNode = FindLast();
+
+                lastNode.Next = newNode;
             }
 
-            //pre 뒤에 data 넣기
-            public void InsertAfter(int previous, int data)
-            {
-                Node preNode = null;
-
-                for (Node temp = head; temp != null; temp = temp.next)
-                {
-                    if (previous == temp.data)
-                    {
-                        preNode = temp;
-                    }
-                }
-
-                if (preNode == null)
-                {
-                    Console.WriteLine($"{previous}는 존재하지 않음");
-                    return;
-                }
-
-                Node node = new Node(data);
-                node.next = preNode.next;
-                preNode.next = node;
-            }
-
-            public void DeleteNode(int key)
-            {
-                Node temp = head;
-                Node previousNode = null;
-
-                if (temp != null && temp.data == key)
-                {
-                    head = temp.next;
-                    return;
-                }
-
-                while (temp != null && temp.data != key)
-                {
-                    previousNode = temp;
-                    temp = temp.next;
-                }
-                if (temp == null)
-                {
-                    return;
-                }
-
-                previousNode.next = temp.next;
-            }
-
-            public void Reverse()
-            {
-                Node previousNode = null;
-                Node currentNode = head;
-                Node temp = null;
-
-                while (currentNode != null)
-                {
-                    temp = currentNode;
-                    currentNode.next = previousNode;
-                    previousNode = currentNode;
-                    currentNode = temp;
-                }
-            }
-
-            public void Print()
-            {
-
-            }
+            //Clear
+            //Contains
+            //CopyTo
+            //Find
+            //Remove
+            //RemoveFirst
+            //RemoveLast
         }
     }
+
+
+    //public class Chapter3LinkeList
+    //{
+    //    public class Node
+    //    {
+    //        public int data { get; set; }
+
+    //        public Node next { get; set; }
+
+    //        public Node(int data)
+    //        {
+    //            this.data = data;
+    //            next = null;
+    //        }
+    //    }
+
+    //    public class LinkedList
+    //    {
+    //        public Node head;
+    //        public Node GetLastNode()
+    //        {
+    //            Node lastNode = head;
+    //            while (lastNode.next != null)
+    //            {
+    //                lastNode = lastNode.next;
+    //            }
+
+    //            return lastNode;
+    //        }
+
+    //        public void InsertFront(int data)
+    //        {
+    //            Node node = new Node(data);
+    //            node.next = head;
+    //            head = node;
+    //        }
+
+    //        public void InsertLast(int data)
+    //        {
+    //            Node node = new Node(data);
+
+    //            if (head == null)
+    //            {
+    //                head = node;
+    //                return;
+    //            }
+
+    //            Node lastNode = GetLastNode();
+    //            lastNode.next = node;
+    //        }
+
+    //        //pre 뒤에 data 넣기
+    //        public void InsertAfter(int previous, int data)
+    //        {
+    //            Node preNode = null;
+
+    //            for (Node temp = head; temp != null; temp = temp.next)
+    //            {
+    //                if (previous == temp.data)
+    //                {
+    //                    preNode = temp;
+    //                }
+    //            }
+
+    //            if (preNode == null)
+    //            {
+    //                Console.WriteLine($"{previous}는 존재하지 않음");
+    //                return;
+    //            }
+
+    //            Node node = new Node(data);
+    //            node.next = preNode.next;
+    //            preNode.next = node;
+    //        }
+
+    //        public void DeleteNode(int key)
+    //        {
+    //            Node temp = head;
+    //            Node previousNode = null;
+
+    //            if (temp != null && temp.data == key)
+    //            {
+    //                head = temp.next;
+    //                return;
+    //            }
+
+    //            while (temp != null && temp.data != key)
+    //            {
+    //                previousNode = temp;
+    //                temp = temp.next;
+    //            }
+    //            if (temp == null)
+    //            {
+    //                return;
+    //            }
+
+    //            previousNode.next = temp.next;
+    //        }
+
+    //        public void Reverse()
+    //        {
+    //            Node previousNode = null;
+    //            Node currentNode = head;
+    //            Node temp = null;
+
+    //            while (currentNode != null)
+    //            {
+    //                temp = currentNode;
+    //                currentNode.next = previousNode;
+    //                previousNode = currentNode;
+    //                currentNode = temp;
+    //            }
+    //        }
+
+    //        public void Print()
+    //        {
+
+    //        }
+    //    }
+    //}
 }
--- Program.cs
+++ Program.cs
@@ -1,74 +1,68 @@
 ﻿using System;
 
-using static StudyChapter3.Chapter3LinkeList;
-using static StudyChapter3.Chapter3Stack;
-
 namespace StudyChapter3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] a = { 10, 45, 23, 5, 212, 65, 1, 2 };
+            //int[] a = { 10, 45, 23, 5, 212, 65, 1, 2 };
 
             //PrintArray<int>("a[] :", a);
             //CalcArray<int>(a);
 
-            LinkedList list = new LinkedList();
-            Random random = new Random();
+            //LinkedList list = new LinkedList();
+            //Random random = new Random();
 
-            for (int i = 0; i < 5; i++)
-            {
-                list.InsertLast(random.Next(100));
-            }
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    list.InsertLast(random.Next(100));
+            //}
 
-            list.InsertFront(10);
-            list.InsertLast(90);
-            list.InsertAfter(10, 11);
-            list.DeleteNode(90);
+            //list.InsertFront(10);
+            //list.InsertLast(90);
+            //list.InsertAfter(10, 11);
+            //list.DeleteNode(90);
 
-            MyStack<int> stack = new MyStack<int>();
+            //MyStack<int> stack = new MyStack<int>();
 
-            for (int i = 0; i < 10; i++)
-            {
-                int value = random.Next(100);
-                Console.WriteLine($"Stack Push: {value}");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int value = random.Next(100);
+            //    Console.WriteLine($"Stack Push: {value}");
 
-                stack.Push(value);
-            }
+            //    stack.Push(value);
+            //}
 
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"Stack Pop: {stack.Pop()}");
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"Stack Pop: {stack.Pop()}");
+            //}
 
-            int[] array = new int[30];
+            //int[] array = new int[30];
 
-            //IComparable reverseComparer = new ReverseComparer();
-            string.Compare
         }
 
+        //private static void CalcArray<T>(T[] array) where T : struct
+        //{
+        //    T sum = default(T);
+        //    T avg = default(T);
+        //    var max = default(T);
 
-        private static void CalcArray<T>(T[] array) where T : struct
-        {
-            T sum = default(T);
-            T avg = default(T);
-            var max = default(T);
+        //    foreach (dynamic item in array)
+        //    {
 
-            foreach (dynamic item in array)
-            {
+        //    }
+        //}
 
-            }
-        }
+        //private static void PrintArray<T>(string s, T[] intArray) where T : struct
+        //{
+        //    Console.Write($"{s}");
 
-        private static void PrintArray<T>(string s, T[] intArray) where T : struct
-        {
-            Console.Write($"{s}");
-
-            foreach (var intItem in intArray)
-            {
-                Console.Write($"{intItem}");
-            }
-        }
+        //    foreach (var intItem in intArray)
+        //    {
+        //        Console.Write($"{intItem}");
+        //    }
+        //}
     }
 }
